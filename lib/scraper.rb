683ace5ac37bee5bc2a614c638253cca5cb00d20
Amber Taranto <ambzzzz@gmail.com>
2019-08-11 01:00:48 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,7 +2,7 @@ require 'nokogiri'
 require 'open-uri'
 
 doc = Nokogiri::HTML(open("https://flatironschool.com/"))
-puts doc.css(".headline-26OIBN")
+doc.css(".headline-26OIBN").text
 
 
 # <h1 class="headline-26OIBN">Change things.</h1>
